f2e7eb9aa5395e462fd43113f9a02de34c21b92d
morozovakay-1991 <morozova.kay@gmail.com>
2026-09-07 16:52:11 +0400

    fix: исправлен docblock @use в User.php для PHPStan

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,10 +14,11 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasApiTokens;
 
+    /** @use HasFactory<UserFactory> */
     use HasFactory;
+
     use Notifiable;
 
     /**
